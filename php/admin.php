e3bfd725b4964af2d6a6090b13f73e1fd142f6b6
Vitor Izidoro <vitorizidoro123@gmail.com>
2023-10-29 15:42:17 -0300

    os arquivos são nomeados automaticamente como o numero do "ID_jogo

--- php/admin.php
+++ php/admin.php
@@ -1,27 +1,32 @@
-<?php 
+<?php
+$nome = $_POST['nome'];
+$preco = $_POST['preco'];
+$descricao = $_POST['genero'];
+$arquivo = $_FILES["arquivo"];
+$mensagem = "É necessário que o arquivo seja um PNG";
 
-    $nome=$_POST['nome'];
-    $preco=$_POST['preco'];
-    $descricao=$_POST['genero'];
-    $arquivo = $_FILES["arquivo"];
-    $mensagem = "é necessario que o arquivo seja um png";
+if ($arquivo["type"] == "image/png") {
+    $novo_endereco = "../images/" . $nome . ".png";
+    $mensagem = "Arquivo enviado";
+
+    move_uploaded_file($arquivo["tmp_name"], $novo_endereco);
+    $conection__db = mysqli_connect('localhost:3306', 'root', 'C0x1nh4123', 'honeygame__db');
+
+ 
+    $query = "INSERT INTO jogo (nome, preco, descricao) VALUES ('$nome','$preco','$descricao')";
+    mysqli_query($conection__db, $query);
 
 
-    if($arquivo["type"] == "image/png")
-    {
-        $novo_endereco = "../images/" . $nome .".png";
-        $mensagem= "arquivo enviado";
-        
-        move_uploaded_file($arquivo["tmp_name"], $novo_endereco);
-        $conection__db = mysqli_connect('localhost:3306', 'root', 'C0x1nh4123','honeygame__db');
-        $receber = mysqli_query($conection__db, "SELECT ID_jogos FROM jogo");
-        $query = "INSERT INTO jogo (nome, preco, descricao) VALUES ('$nome','$preco','$descricao')";
-        mysqli_query($conection__db, $query);
+    $ultimo_id_inserido = mysqli_insert_id($conection__db);
 
-    }
-    else{
-        $mensagem= "o arquivo deve obrigatoriamente um png";
-    }
-    echo json_encode(["mensagem" => $mensagem]);
-  
-?>  
+    // Renomear o arquivo com o ID do jogo
+    $novo_nome_arquivo = $ultimo_id_inserido . ".png";
+    $novo_endereco_com_id = "../images/" . $novo_nome_arquivo;
+    rename($novo_endereco, $novo_endereco_com_id);
+
+} else {
+    $mensagem = "O arquivo deve obrigatoriamente ser um PNG";
+}
+
+echo json_encode(["mensagem" => $mensagem]);
+?>
